b3d03c37c7a36121159881d9f37cf8cc2307c31b
Malik rayane <rayane@pop-os.localdomain>
2024-04-23 14:35:18 +0200

    evolution

--- test.py
+++ test.py
@@ -13,17 +13,20 @@ def determiner_type_aliment(nom_aliment):
         'vegetable': ['carotte', 'tomate', 'brocoli', 'épinard', 'concombre', 'poivron', 'courgette','pomme de terre',
                        'courge', 'aubergine', 'asperge', 'radis', 'chou-fleur', 'haricot vert' ],
         'nuts': ['amande', 'noisette', 'noix', 'pistache', ],
-        'dairy': ['lait', 'fromage', 'yaourt', 'beurre', 'crème', 'kéfir'],
-        'egg': ['œuf', 'omelette', 'frittata', 'soufflé', 'meringue'],
+        'dairy': ['lait', 'fromage', 'yaourt', 'beurre', 'crème', 'kéfir', 'féta'],
+        'egg': ['œuf', 'omelette', 'frittata', 'soufflé', 'meringue', 'oeuf'],
         'seafood': ['crevette', 'saumon', 'thon', 'huître', 'moule', 'crabe', 'homard', 'calamar', 'palourde', 'langouste',
-                    'saint-Jacques', 'anguille'],
+                    'saint-Jacques', 'anguille', 'éperlan'],
         'cereal': ['blé', 'riz', 'maïs', 'avoine', 'quinoa', 'millet', 'épeautre', 'sarrasin', 'seigle', 'orge', 'boulgour',
                     'pâtes'],
         'boisson': ['eau', 'jus', 'café', 'thé']
     }
 
     # Liste des préfixes à ignorer
-    prefixes_a_ignorer = ['au ', 'à ', 'aux']
+    prefixes_a_ignorer = ['au ', 'à']
+
+    # Liste des lettres à ignorer en fin de mot
+    pluriel = ['s', 'x']
 
     # Convertir le nom de l'aliment en minuscules pour la correspondance insensible à la casse
     nom_aliment_lower = nom_aliment.lower()
@@ -32,9 +35,12 @@ def determiner_type_aliment(nom_aliment):
 
     for element in types.values():
         for item in element:
+            #print(item)
             # Vérifie si l'élément est dans le nom de l'aliment et si son préfixe n'est pas à ignorer
             if item in nom_aliment_lower and not any(prefix in nom_aliment_lower for prefix in prefixes_a_ignorer):
-                elements_present.add(item)
+                if f' {item} ' in f' {nom_aliment_lower} ': 
+                    elements_present.add(item)
+    print(elements_present)
           
       # Vérifiez si un élément contient un autre
     for element1 in elements_present:
@@ -47,7 +53,7 @@ def determiner_type_aliment(nom_aliment):
 
     # Si aucun élément ne contient un autre, retourner simplement un élément présent dans le nom
     if elements_present:
-        print(elements_present)
+        #print(elements_present)
         return elements_present.pop()
 
     return 'autre'
